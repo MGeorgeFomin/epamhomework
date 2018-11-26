3a519c9f3b3e77968a86d91aaffbaa15e079b237
MGeorgeFomin <43377662+MGeorgeFomin@users.noreply.github.com>
2018-11-26 11:14:14 +0300

    add bubble sort class

---
+++ src/main/java/com/algorithms/BubbleSort.java
@@ -0,0 +1,26 @@
+package com.algorithms;
+
+/**
+ * BubbleSort
+ *
+ * @author George Fomin
+ * @version 1.0
+ */
+public class BubbleSort {
+    /**
+     * Method which initialise bubbleSort
+     *
+     * @param inputArray the input array of ints
+     */
+    public void sort(int[] inputArray) {
+        for (int i = 0; i < inputArray.length - 1; i++) {
+            for (int j = 0; j < inputArray.length - 1; j++) {
+                if (inputArray[j] > inputArray[j + 1]) {
+                    int countToSwap = inputArray[j];
+                    inputArray[j] = inputArray[j + 1];
+                    inputArray[j + 1] = countToSwap;
+                }
+            }
+        }
+    }
+}
