fd19eaf1c86f5f8ec243944fd9411fdba7bae60d
MGeorgeFomin <43377662+MGeorgeFomin@users.noreply.github.com>
2018-11-26 11:18:23 +0300

    add merge sort class

---
+++ src/main/java/com/algorithms/MergeSort.java
@@ -0,0 +1,72 @@
+package com.algorithms;
+
+/**
+ * MergeSort
+ *
+ * @author George Fomin
+ * @version 1.0
+ */
+public class MergeSort {
+    private int[] inputArray;
+    private int[] helperArray;
+
+    /**
+     * Method which initialise the Merge sorting
+     *
+     * @param inputArray the input array of ints
+     */
+    public void sort(int[] inputArray) {
+        this.inputArray = inputArray;
+        this.helperArray = new int[inputArray.length];
+        mergeSort(0, inputArray.length - 1);
+    }
+
+    /**
+     * Method which separate parts of InputArray for sorting by pairs
+     *
+     * @param lowerBoundary  the low bound
+     * @param higherBoundary the high bound
+     */
+    private void mergeSort(int lowerBoundary, int higherBoundary) {
+        if (lowerBoundary < higherBoundary) {
+            int middleBoundary = lowerBoundary + (higherBoundary - lowerBoundary) / 2;
+            mergeSort(lowerBoundary, middleBoundary);
+            mergeSort(middleBoundary + 1, higherBoundary);
+            merge(lowerBoundary, middleBoundary, higherBoundary);
+        }
+    }
+
+    /**
+     * Method which sort and merge the sub arrays
+     *
+     * @param lowerBoundary  the low bound
+     * @param middleBoundary the middle bound
+     * @param higherBoundary the high bound
+     */
+    private void merge(int lowerBoundary, int middleBoundary, int higherBoundary) {
+        if (higherBoundary + 1 - lowerBoundary >= 0)
+            System.arraycopy(this.inputArray, lowerBoundary,
+                    this.helperArray, lowerBoundary, higherBoundary + 1 - lowerBoundary);
+
+        int i = lowerBoundary;
+        int j = middleBoundary + 1;
+        int k = lowerBoundary;
+
+        while (i <= middleBoundary && j <= higherBoundary) {
+            if (this.helperArray[i] <= this.helperArray[j]) {
+                this.inputArray[k] = this.helperArray[i];
+                i++;
+            } else {
+                this.inputArray[k] = this.helperArray[j];
+                j++;
+            }
+            k++;
+        }
+
+        while (i <= middleBoundary) {
+            this.inputArray[k] = this.helperArray[i];
+            k++;
+            i++;
+        }
+    }
+}
